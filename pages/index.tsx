7bc05e766db556055ac70e1854f9b2f45c71c8dc
Kui Ádám <adamdaniel.kui@erstebank.hu>
2022-09-21 14:02:06 +0200

    Add video for home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,47 @@
+import { Typography } from "@mui/material";
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import { useEffect, useState } from "react";
+import Typed from "react-typed";
 
 const Home: NextPage = () => {
+  let typed: any;
+
+  useEffect(() => {
+    if (typed) {
+      typed.start();
+    }
+  }, [typed]);
+
   return (
+    <div>
+      <main>
+        <section className="mb-5 h-96 flex relative justify-center items-center text-center px-10">
+          <Typography variant={"h4"}>
+            <Typed
+              typedRef={(typedRef: any) => (typed = typedRef)}
+              strings={[
+                "Welcome.",
+                "My name is Adam Kui.",
+                "I'm a software developer from Budapest, Hungary.",
+                "Take a look around!",
+              ]}
+              startDelay={500}
+              typeSpeed={25}
+              backSpeed={5}
+              backDelay={1750}
+              loop
+            />
+          </Typography>
+          <video
+            src="river_loop.mp4"
+            autoPlay
+            loop
+            muted
+            className="min-w-full min-h-full object-cover opacity-50 absolute left-0 top-0 h-96"
+          />
+        </section>
+      </main>
+    </div>
     /*
     <div className={styles.container}>
       <Head>
@@ -67,7 +105,6 @@ const Home: NextPage = () => {
       </footer>
     </div>
     */
-    null
   );
 };
 
