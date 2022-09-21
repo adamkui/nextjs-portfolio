2237673cb824c867ffe325b8d8bb384fecfccec0
Kui Ádám <adamdaniel.kui@erstebank.hu>
2022-09-21 20:23:26 +0200

    WIP main page

--- components/AboutMe/AboutMe.tsx
+++ components/AboutMe/AboutMe.tsx
@@ -5,8 +5,10 @@ export const AboutMe: FC = () => {
   return (
     <Section
       title="What I ❤️️"
-      body="test"
-      className="flex flex-col w-full"
+      body={
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo tenetur nobis accusamus culpa expedita, laudantium dolores asperiores reprehenderit deleniti assumenda maxime voluptatem quod earum officia omnis ab est facere nihil nemo neque tempora incidunt. Quisquam accusamus inventore eum iste nulla iusto suscipit est id sapiente delectus animi amet a aliquid, rerum libero quaerat saepe beatae nam voluptates nihil consequatur accusantium?"
+      }
+      className="flex flex-col w-full mb-20"
     ></Section>
   );
 };
