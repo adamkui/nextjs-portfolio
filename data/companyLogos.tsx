4b0f9cb4009b4eeab1040e444ee7a1c05f470159
Kui Ádám <adamdaniel.kui@erstebank.hu>
2022-09-25 02:38:31 +0200

    jpg to webp

--- data/companyLogos.tsx
+++ data/companyLogos.tsx
@@ -5,8 +5,8 @@ interface CompanyLogo {
 }
 
 export const companyLogos: CompanyLogo[] = [
-  { img: <img src="erste-logo.png" alt="Erste_Logo" className="p-7" /> },
-  { img: <img src="abg-logo.png" alt="Abg_Logo" /> },
-  { img: <img src="exxon-logo.png" alt="Exxon_Logo" className="pt-3" /> },
-  { img: <img src="ge-logo.png" alt="Ge_Logo" className="p-10" /> },
+  { img: <img src="erste-logo.webp" alt="Erste_Logo" className="p-7" /> },
+  { img: <img src="abg-logo.webp" alt="Abg_Logo" /> },
+  { img: <img src="exxon-logo.webp" alt="Exxon_Logo" className="pt-3" /> },
+  { img: <img src="ge-logo.webp" alt="Ge_Logo" className="p-10" /> },
 ];
