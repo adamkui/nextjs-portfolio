e372b654d4d6fc6b630e835f7a821f84fe779871
Kui Ádám <adamdaniel.kui@erstebank.hu>
2022-10-01 16:20:08 +0200

    Update [id].tsx

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,7 +1,18 @@
 import { NextPage } from "next";
+import { useRouter } from "next/router";
+
+import { Section } from "components";
+import { useGetText } from "hooks";
 
 const BlogArticlePage: NextPage = () => {
-  return <div>Asd</div>;
+  const t = useGetText();
+  const router = useRouter();
+
+  const blogArticleId = router.query.id;
+
+  console.log(blogArticleId);
+
+  return <Section title={t("ABOUT_ME_BODY_1")} body={"asdasdadads"}></Section>;
 };
 
 export default BlogArticlePage;
