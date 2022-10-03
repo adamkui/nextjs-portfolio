01943ca642834402073be7c1e44156e7c7502088
Kui Ádám <adamdaniel.kui@erstebank.hu>
2022-10-04 01:00:41 +0200

    Font optimalization

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,11 +4,17 @@ const Document = () => {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.svg" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.svg" />
         <link rel="preload" as="image" href="/assets/images/profile.webp" />
         <link rel="preload" as="favicon" href="/favicon.svg" />
-        <link rel="preload" as="font" href="/fonts/Gugi-Regular.ttf" />
+        <link
+          rel="preload"
+          as="font"
+          type="font/woff2"
+          href="fonts/Gugi-Regular.woff2"
+          crossOrigin=""
+        />
       </Head>
       <body>
         <Main />
