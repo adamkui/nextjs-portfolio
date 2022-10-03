67297a574fb4f47508a7e84f520716dc9d237093
Kui Ádám <adamdaniel.kui@erstebank.hu>
2022-10-03 23:48:44 +0200

    Performance upgrades

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,11 @@ const Document = () => {
       <Head>
         <link rel="icon" href="/favicon.svg" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="preload"
+          as="image"
+          href="https://adamkui-nextjs-portfolio.netlify.app/assets/images/profile.webp"
+        ></link>
       </Head>
       <body>
         <Main />
